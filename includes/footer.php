8b503dcf43290b9020def31c8a768451a240bef9
ychia0612-dotcom <ychia0612@gmail.com>
2026-05-15 09:30:22 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -6,15 +6,22 @@ include_once 'db.php';
 recordVisit($conn);
 
 // 只有首頁顯示總訪問人次
-if (basename($_SERVER['PHP_SELF']) === 'index.php') {
+$show_visits = (basename($_SERVER['PHP_SELF']) === 'index.php');
+if ($show_visits) {
     $total_visits = getTotalVisits($conn);
-    echo '<div style="text-align:center; margin:10px 0;">🌐 網站總訪問人次：' . number_format($total_visits) . '</div>';
 }
 $conn->close();
 ?>
 
-<div style="text-align:center; margin:10px 0;">
-    © 2026 SDG12 永續生活家學習平台
-</div>
+<footer class="footer">
+    <?php if ($show_visits): ?>
+        <div style="margin-bottom: 15px; font-size: 16px; color: var(--text-light);">
+            🌐 網站總訪問人次：<?php echo number_format($total_visits); ?>
+        </div>
+    <?php endif; ?>
+    <div style="text-align: center; width: 100%;">
+        © 2026 SDG12 永續生活家學習平台
+    </div>
+</footer>
 
 <script src="assets/js/modal.js"></script>
